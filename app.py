c988116a4e58212b8211edc44107376ce773d009
beggainer-crypto <73869007+beggainer-crypto@users.noreply.github.com>
2020-11-03 09:15:36 +0400

    calculator

---
+++ app.py
@@ -0,0 +1,21 @@
+
+
+num1 = float(input('number ='))
+num2 = float(input('number 2'))
+operation = input('')
+if operation == "+":
+    print(str(num1+num2))
+elif operation == "-":
+    print(str(num1 - num2))
+elif operation == "x":
+    print(str(num1 * num2))
+elif operation == "/":
+    print(str(num1 // num2))
+elif operation == '**':
+    print(str(num1 * num1))
+elif operation == 'X':
+    print(str(num1 * num2))
+elif operation == '*':
+    print(str(num1 * num2))
+
+
